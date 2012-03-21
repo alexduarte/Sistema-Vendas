4321b8f97e7b6c5da598adb34b1958b1c7c4f6c3
alexsander <avuldarte@gmail.com>
2012-03-21 20:43:07 -0300

    Projeto criado para a disciplina de LTPIII

---
+++ src/br/edu/fasa/vendas/domainmodel/Cliente.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasa.vendas.domainmodel;
+
+/**
+ *
+ * @author Alexsander Duarte Pimenta
+ */
+public class Cliente {
+    
+    private int pkCod;
+    private String nome;
+
+    
+    public int getPkCod() {
+        return pkCod;
+    }
+
+    /**
+     * @param pkCod the pkCod to set
+     */
+    public void setPkCod(int pkCod) {
+        this.pkCod = pkCod;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+    
+}
