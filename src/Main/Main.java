5947ba7752fcaa255da02d454ba4752e7fe30d2e
alexsander <avuldarte@gmail.com>
2012-04-15 17:07:48 -0300

    Classse principal

---
+++ src/Main/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import br.edu.fasa.vendas.domainmodel.Cliente;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author cliente
+ */
+public class Main {
+    
+    public static void main(String args[]){
+        
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("VendasPU");
+        EntityManager manager = factory.createEntityManager();
+        Cliente cli = new Cliente();
+        cli.setNome("Alex");
+        EntityTransaction tran = manager.getTransaction();
+        tran.begin();
+        manager.persist(cli);
+        tran.commit();
+        
+        
+
+    }
+    
+}
